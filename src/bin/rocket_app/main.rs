14badb660a5a2158c689549f2a1df428fcb7b989
philipwastakenwastaken <37301670+philipwastakenwastaken@users.noreply.github.com>
2023-12-08 18:01:53 +0100

    Moved scss compilation to its own module with custom error types

--- src/bin/rocket_app/main.rs
+++ src/bin/rocket_app/main.rs
@@ -2,14 +2,13 @@
 extern crate rocket;
 extern crate grass;
 extern crate tera;
+mod scss;
 
-use std::env;
+use scss::compile;
 
 use rocket::fs::FileServer;
 use rocket::response::Redirect;
 use rocket_dyn_templates::Template;
-use std::fs::File;
-use std::io::Write;
 use tera::Context;
 
 #[catch(404)]
@@ -23,19 +22,14 @@ fn home() -> Template {
     Template::render("home", &context)
 }
 
-// No native support for SCSS in Rocket
-fn compile_scss() {
-    let css_path = env::var("MAIN_CSS_PATH").expect("Could not retrieve MAIN_CSS_PATH env. variable");
-    let scss_path = env::var("MAIN_SCSS_PATH").expect("Could not retrieve MAIN_SCSS_PATH env. variable");
-    let mut output_file = File::create(css_path).expect("Problem creating file object");
-    let css = grass::from_path(scss_path, &grass::Options::default())
-        .expect("Problem opening the file");
-    let _result = write!(output_file, "{}", css).expect("Write operation failed");
-}
-
 #[launch]
 fn rocket() -> _ {
-    compile_scss();
+    let _compile_result = {
+        match compile("MAIN_SCSS_PATHS", "MAIN_CSS_PATH") {
+            Ok(()) => (),
+            Err(error) => panic!("SCSS compilation failed: {}", error),
+        }
+    };
 
     rocket::build()
         .mount("/", routes![home])
---
+++ src/bin/rocket_app/scss.rs
@@ -0,0 +1,37 @@
+use thiserror::Error;
+use std::env::{VarError, self};
+use std::fs::File;
+use std::io::Write;
+use grass;
+
+#[derive(Error, Debug)]
+pub enum CompilationError {
+    #[error("Environment variable not set")]
+    EnvironmentError(#[from] VarError),
+
+    #[error("Could not create file")]
+    FileCreateError(#[source] std::io::Error),
+    #[error("Could not write to file")]
+    FileWriteError(#[source] std::io::Error),
+
+    #[error("{}", .0.to_string())]
+    Failed(#[from] Box<grass::Error>)
+}
+
+// No native support for SCSS in Rocket.
+// Paths are also used in Tera templates, so environment variables are
+// used to share state (see .cargo/config.toml).
+pub fn compile(scss_path_key: &str, css_path_key: &str) -> Result<(), CompilationError> {
+    use CompilationError::{FileCreateError, FileWriteError};
+
+    let css_path = env::var(scss_path_key)?;
+    let scss_path = env::var(css_path_key)?;
+    let mut output_file = File::create(css_path)
+            .map_err(FileCreateError)?;
+
+    let css = grass::from_path(scss_path, &grass::Options::default())?;
+    write!(output_file, "{}", css).map_err(FileWriteError)?;
+
+    Ok(())
+}
+
