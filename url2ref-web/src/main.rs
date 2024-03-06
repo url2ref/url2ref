b2bd953f315cbc4f4da9e0692abdd700dcb98fe0
Philip Tofte Nielsen <s174274@student.dtu.dk>
2024-03-06 12:58:00 +0100

    MVP for web app
    This commit adds minimal functionality to the web app.
    There is a very simple interface, that given an URL
    and a citation format, will output the given citation.
    There is still a good amount of work to be done. The UI
    design is lacklustre and does not support all the features of our
    backend.

--- url2ref-web/src/main.rs
+++ url2ref-web/src/main.rs
@@ -1,21 +1,71 @@
+mod form;
 mod scss;
+use form::{CITATION_FORMATS, LANGUAGE_CODES};
+use rocket::form::Form;
 use scss::compile;
 
 use rocket::fs::FileServer;
 use rocket::response::Redirect;
-use rocket::{self, catch, catchers, get, launch, routes, uri};
+use rocket::{catch, catchers, get, launch, post, routes, uri};
 use rocket_dyn_templates::Template;
 use tera::Context;
+use url2ref::generator::attribute_config::AttributeConfig;
+use url2ref::generator::TranslationOptions;
+use url2ref::{generate, GenerationOptions};
+
+use crate::form::{ReferenceInput, BIBTEX_FORMAT, WIKI_FORMAT};
 
 #[catch(404)]
 fn not_found() -> Redirect {
     Redirect::to(uri!(home))
 }
 
+fn render_home(mut context: Context, current_url: &String) -> Template {
+    context.insert("current_url", &current_url);
+    context.insert("language_codes", LANGUAGE_CODES);
+    context.insert("citation_formats", CITATION_FORMATS);
+
+    Template::render("home", &context.into_json())
+}
+
 #[get("/")]
 fn home() -> Template {
-    let context = Context::new().into_json();
-    Template::render("home", &context)
+    render_home(Context::new(), &"".to_string())
+}
+
+#[post("/submit_url", data = "<input>")]
+fn submit_url(input: Form<ReferenceInput>) -> Template {
+    println!("{:?}", input);
+
+    let mut context = Context::new();
+    let deepl_key = None;
+
+    let translation_options = TranslationOptions {
+        source: Some(input.source_lang.clone()),
+        target: Some(input.target_lang.clone()),
+        deepl_key: deepl_key,
+    };
+
+    let attribute_config = AttributeConfig::default();
+
+    let generation_options = GenerationOptions {
+        attribute_config,
+        translation_options,
+    };
+
+    let reference_option = generate(&input.url, &generation_options);
+    println!("{:?}", reference_option);
+    if let Ok(reference) = reference_option {
+        let output = match input.format.as_str() {
+            BIBTEX_FORMAT => reference.bibtex(),
+            WIKI_FORMAT => reference.wiki(),
+            _ => panic!(),
+        };
+        
+        context.insert("reference_output", &output);
+    };
+
+    render_home(context, &input.url)
 }
 
 #[launch]
@@ -28,7 +78,7 @@ fn rocket() -> _ {
     };
 
     rocket::build()
-        .mount("/", routes![home])
+        .mount("/", routes![home, submit_url])
         .mount("/static", FileServer::from("./static"))
         .attach(Template::fairing())
         .register("/", catchers![not_found])
--- url2ref-web/static/custom/custom.js
+++ url2ref-web/static/custom/custom.js
@@ -1,21 +1,34 @@
 // Tooltip functionality
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-console.log(tooltipTriggerList)
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
+    return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
 // Copy to clipboard
-function copyToClipboard(msg) {
-    navigator.clipboard.writeText(msg)
+function copyToClipboard() {
+    const reference = document.getElementById("raw_tab_text");
+    navigator.clipboard.writeText(reference.textContent);
 }
 
 // Theme switcher
-document.getElementById('bd-theme').addEventListener('click',()=>{
+document.getElementById('bd-theme').addEventListener('click', () => {
     if (document.documentElement.getAttribute('data-bs-theme') == 'dark') {
-        document.documentElement.setAttribute('data-bs-theme','light')
+        document.documentElement.setAttribute('data-bs-theme', 'light')
     }
     else {
-        document.documentElement.setAttribute('data-bs-theme','dark')
+        document.documentElement.setAttribute('data-bs-theme', 'dark')
     }
 })
+
+/*
+// Prevents POST from actually submitting and reloading page.
+const formElement = document.getElementById("url_form");
+formElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(formElement);
+  fetch('/submit_url', {
+    method: 'POST',
+    body: formData,
+  });
+});
+*/
